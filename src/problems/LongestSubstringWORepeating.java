a4d0fa4b2d4da62a36f39745e9559736f188c9f5
Yi-Ubuntu <alyiwang@yahoo.com>
2014-10-18 16:59:27 -0400

    Update Longest Substring Without Repeating

--- src/problems/LongestSubstringWORepeating.java
+++ src/problems/LongestSubstringWORepeating.java
@@ -1,33 +1,26 @@
 package problems;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class LongestSubstringWORepeating {
     public int lengthOfLongestSubstring(String s) {
         int len = s.length();
         int l = 0, r = 0;
         int max = 0; // max length
-        HashMap<Character, Character> hs = new HashMap<Character, Character>();
+        HashSet<Character> hs = new HashSet<Character>();
 
-        char c = 0, d = 0;
         for (r = 0; r < len; r++) {
-            c = s.charAt(r);
-            if (!hs.containsKey(c)) { // no such char in substring yet
-                hs.put(c, c);
-            } else { // char repeat
-                if (r - l > max)
-                    max = r - l;
-                while ((d = s.charAt(l)) != c) { // search the repeat one
-                    hs.remove(d);
-                    l++;
-                }
+            char c = s.charAt(r);
+            if (!hs.contains(c)) // no such char in substring yet
+                hs.add(c);
+            else { // char repeat
+                max = Math.max(r - l, max);
+                while (s.charAt(l) != c) // search the repeat one
+                    hs.remove(s.charAt(l++));
                 l++; // go to the first non-repeat one
             }
         }
-        if (r - l > max)
-            max = r - l;
-
-        return max;
+        return Math.max(r - l, max);
     }
 
     public static void main(String[] args) {
