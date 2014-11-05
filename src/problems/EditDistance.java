52d02bfd70feb7d5cbc70a7473a4e3028d1ca1a1
Yi-Ubuntu <alyiwang@yahoo.com>
2014-11-05 09:14:21 -0500

    Update Edit Distance and Insert Intervals

--- src/problems/EditDistance.java
+++ src/problems/EditDistance.java
@@ -4,7 +4,7 @@ public class EditDistance {
     public int minDistance(String word1, String word2) {
         if (word1 == null || word2 == null)
             return 0;
-        
+
         int n1 = word1.length(), n2 = word2.length();
         int[][] d = new int[n1 + 1][n2 + 1];
         for (int i = 1; i <= n1; i++)
@@ -19,10 +19,10 @@ public class EditDistance {
         }
         return d[n1][n2];
     }
-    
+
     public static void main(String[] args) {
         EditDistance ed = new EditDistance();
-        
+
         String s1 = "ab", s2 = "bc";
         System.out.println(ed.minDistance(s1, s2));
     }
